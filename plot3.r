950b45b5d3f5ed6250e775205ab0f84db928e21d
arousos <angelo.rousos@gmail.com>
2015-05-09 14:43:43 -0400

    Tidied source retrieval code

--- plot3.r
+++ plot3.r
@@ -1,33 +1,20 @@
-if (file.exists("household_power_consumption.txt")){
-  
-  # Import data
-  lines <- grep("1/2/2007", readLines("household_power_consumption.txt"))
-  skips <- sum(lines[1],-1)
-  power <- read.table("household_power_consumption.txt", sep=";", skip=skips, nrows=2880)
-  names(power) <- c("date","time","globalActivePower","globalReactivePower","voltage","globalIntensity","subMetering1","subMetering2","subMetering3")
-  power$dateTime <- paste(power$date, power$time, sep=" ")
-  power$dateTime <- strptime(power$dateTime, "%d/%m/%Y %H:%M:%S")
-  power <- power[,c(10, 3:9)]
-  
-} else {
-  
-  #Download data
+# Retrieve source
+if (!file.exists("household_power_consumption.txt")){
   zipURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
   download.file(zipURL, destfile = "Electric_power_consumption.zip", mode="wb")
-  unzip("Electric_power_consumption.zip")
-  
-  # Import data
-  lines <- grep("1/2/2007", readLines("household_power_consumption.txt"))
-  skips <- sum(lines[1],-1)
-  power <- read.table("household_power_consumption.txt", sep=";", skip=skips, nrows=2880)
-  names(power) <- c("date","time","globalActivePower","globalReactivePower","voltage","globalIntensity","subMetering1","subMetering2","subMetering3")
-  power$dateTime <- paste(power$date, power$time, sep=" ")
-  power$dateTime <- strptime(power$dateTime, "%d/%m/%Y %H:%M:%S")
-  power <- power[,c(10, 3:9)]
-  
+  unzip("Electric_power_consumption.zip")  
 }
 
-# Plot Line Graph
+# Import data
+lines <- grep("1/2/2007", readLines("household_power_consumption.txt"))
+skips <- sum(lines[1],-1)
+power <- read.table("household_power_consumption.txt", sep=";", skip=skips, nrows=2880)
+names(power) <- c("date","time","globalActivePower","globalReactivePower","voltage","globalIntensity","subMetering1","subMetering2","subMetering3")
+power$dateTime <- paste(power$date, power$time, sep=" ")
+power$dateTime <- strptime(power$dateTime, "%d/%m/%Y %H:%M:%S")
+power <- power[,c(10, 3:9)]
+
+# Plot line graph of sub-Metering variables
 png("plot3.png", width=480, height=480)
 with(power, plot(dateTime, subMetering1, type="l", col="black", xlab="", ylab="Energy sub metering", main=""))
 with(power, points(dateTime, subMetering2, type="l", col="red"))
--- plot4.r
+++ plot4.r
@@ -1,33 +1,20 @@
-if (file.exists("household_power_consumption.txt")){
-  
-  # Import data
-  lines <- grep("1/2/2007", readLines("household_power_consumption.txt"))
-  skips <- sum(lines[1],-1)
-  power <- read.table("household_power_consumption.txt", sep=";", skip=skips, nrows=2880)
-  names(power) <- c("date","time","globalActivePower","globalReactivePower","voltage","globalIntensity","subMetering1","subMetering2","subMetering3")
-  power$dateTime <- paste(power$date, power$time, sep=" ")
-  power$dateTime <- strptime(power$dateTime, "%d/%m/%Y %H:%M:%S")
-  power <- power[,c(10, 3:9)]
-  
-} else {
-  
-  #Download data
+# Retrieve source
+if (!file.exists("household_power_consumption.txt")){
   zipURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
   download.file(zipURL, destfile = "Electric_power_consumption.zip", mode="wb")
-  unzip("Electric_power_consumption.zip")
-  
-  # Import data
-  lines <- grep("1/2/2007", readLines("household_power_consumption.txt"))
-  skips <- sum(lines[1],-1)
-  power <- read.table("household_power_consumption.txt", sep=";", skip=skips, nrows=2880)
-  names(power) <- c("date","time","globalActivePower","globalReactivePower","voltage","globalIntensity","subMetering1","subMetering2","subMetering3")
-  power$dateTime <- paste(power$date, power$time, sep=" ")
-  power$dateTime <- strptime(power$dateTime, "%d/%m/%Y %H:%M:%S")
-  power <- power[,c(10, 3:9)]
-  
+  unzip("Electric_power_consumption.zip")  
 }
 
-# Plot 4 Line Graphs
+# Import data
+lines <- grep("1/2/2007", readLines("household_power_consumption.txt"))
+skips <- sum(lines[1],-1)
+power <- read.table("household_power_consumption.txt", sep=";", skip=skips, nrows=2880)
+names(power) <- c("date","time","globalActivePower","globalReactivePower","voltage","globalIntensity","subMetering1","subMetering2","subMetering3")
+power$dateTime <- paste(power$date, power$time, sep=" ")
+power$dateTime <- strptime(power$dateTime, "%d/%m/%Y %H:%M:%S")
+power <- power[,c(10, 3:9)]
+
+# Plot four line graphs in a 2x2 grid : global active power, voltage, submetering, and global reactive power
 png("plot4.png", width=480, height=480)
 par(mfrow= c(2,2))
 with(power, {
